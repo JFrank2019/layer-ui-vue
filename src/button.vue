c88697669bc8891f7b76a52c45587c7c53be8540
jfrank2019 <jfrank2019@163.com>
2021-09-19 21:20:45 +0800

    加入 parcel，将 button 组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="l-button">按钮</button>
+</template>
+
+<script>
+export default {
+    name: 'l-button',
+};
+</script>
+
+<style lang="scss">
+.l-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+
+    &:hover {
+        border-color: var(--border-color-hover);
+    }
+
+    &:active {
+        border-color: var(--button-active-bg);
+    }
+
+    &:focus {
+        outline: none;
+    }
+}
+</style>
